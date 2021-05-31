744ff40faac0e059ca47319126dabfea4b8b6f9f
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2021-05-31 15:05:24 +0700

    Click statistics are now loaded correctly from the API

--- src/modules/Dashboard/components/LinkClicksCard.vue
+++ src/modules/Dashboard/components/LinkClicksCard.vue
@@ -1,31 +1,81 @@
 <template>
   <div class="card p-4">
-    <div class="fs-4">
-      {{ link.short_link }}
+    <div class="row">
+      <div class="col-8">
+        <h3 class="font-weight-bold">
+          {{ link.short_link }}
+        </h3>
+      </div>
+      <div class="col-4 m-auto">
+        <!-- This is where I will put the link-bar -->
+      </div>
     </div>
-    <p>
-      {{link.title}}
-    </p>
-    <p>
+
+    <div>
+      {{link.title}} something
+    </div>
+    <a>
       {{link.url}}
-    </p>
-    <clickStatsChart />
+    </a>
+    <clickStatsChart
+      v-if="state.isLoaded"
+      v-bind:chartData="state.chartData" 
+      :chartOptions="state.chartOptions" 
+    />
   </div>
 </template>
 
 <script>
 import clickStatsChart from './ClickStatsChart.vue'
+import httpAxios from '@/utils/http-axios'
 
 export default {
   name: 'linkClicksCard',
   props: {
     link: Object
   },
+  data() {
+    return {
+      state: {
+        isLoaded: false,
+        chartData: {},
+        chartOptions: {
+          responsive: true
+        }
+      }
+    }
+  },
+  watch: {
+    // call again the method if the route changes
+    '$route': 'getData'
+  },
   components: {
     clickStatsChart
   },
   mounted() {
-    console.log(this.link.id)
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      this.state.isLoaded = false
+      await httpAxios.get(`/short-links/${this.$route.query.id}/stats`)
+        .then((result) => {
+          this.state.chartData = {
+            labels: ['1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7'],
+            datasets: [
+              {
+                label: 'Clicks',
+                backgroundColor: '#f87979',
+                data: result.data.timeSeries
+              }
+            ]
+          }
+          this.state.isLoaded = true
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
   },
 }
 </script>
